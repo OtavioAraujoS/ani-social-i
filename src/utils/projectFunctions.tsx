760edd0d9003b9560d7fcc96c8f5de0eeab96043
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-07 08:17:47 -0300

    feat: navigation with project utilities

---
+++ src/utils/projectFunctions.tsx
@@ -0,0 +1,22 @@
+import { Database, MessageSquare, User } from "lucide-react";
+
+export const projectFunctions = [
+  {
+    icon: <Database className="w-6 h-6" />,
+    title: "Biblioteca de Animes",
+    description: "Acesse o repositório de informações de animes.",
+    href: "#",
+  },
+  {
+    icon: <MessageSquare className="w-6 h-6" />,
+    title: "Fórum de Discussão",
+    description: "Participe de discussões na comunidade.",
+    href: "#",
+  },
+  {
+    icon: <User className="w-6 h-6" />,
+    title: "Hub de Membros",
+    description: "Gerencie seu perfil e configurações.",
+    href: "#",
+  },
+];
---
+++ src/utils/projectLinks.tsx
@@ -0,0 +1,22 @@
+export const projectLinks = [
+  {
+    id: 1,
+    title: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    title: "Animes",
+    link: "#",
+  },
+  {
+    id: 3,
+    title: "Fórum",
+    link: "#",
+  },
+  {
+    id: 4,
+    title: "Comunidade",
+    link: "#",
+  },
+];
