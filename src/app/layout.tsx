f4fe6acef9ef39683ccd1d544d51ec7fdb3b6a1d
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-08 14:44:04 -0300

    refactor: abstract next-themes into a custom ThemeProvider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/providers/AuthProvider";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const inter = Inter({
   variable: "--font-inter",
