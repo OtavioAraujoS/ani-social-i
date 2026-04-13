1a08dc7d1d819b6c1846c6808d7e65ef633a7367
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-13 14:48:54 -0300

    feat: remove cookies from server-side and add 404 image

--- src/services/authUtils.ts
+++ src/services/authUtils.ts
@@ -1,8 +1,5 @@
 import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
 
 export async function handleUnauthorizedServer() {
-  const cookieStore = await cookies();
-  cookieStore.delete("ani-social-token");
   redirect("/login");
 }
