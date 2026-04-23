a5363f53f81fd1a53c02baec5a827760daef5282
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-23 10:25:07 -0300

    fix: time to refresh

--- src/utils/LoadingAndRefresh.ts
+++ src/utils/LoadingAndRefresh.ts
@@ -1,6 +1,6 @@
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export async function LoadingAndRefresh(router: AppRouterInstance) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   router.refresh();
 }
