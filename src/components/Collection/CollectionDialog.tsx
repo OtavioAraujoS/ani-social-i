16b6f65acb0a7b668138267a239b0a8570f36232
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-29 15:08:38 -0300

    fix: refactor collection dialog component

--- src/components/Collection/CollectionDialog.tsx
+++ src/components/Collection/CollectionDialog.tsx
@@ -1,32 +1,19 @@
 "use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
 import { CollectionForm } from "./CollectionForm";
-import { DialogTitleStyled } from "../DialogTitleStyled";
 import { useState } from "react";
 import { RegisterButton } from "../RegisterButton";
+import { StyledDialog } from "../StyledDialog";
 
 export function CollectionDialog() {
   const [isLoading, setIsLoading] = useState(false);
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <RegisterButton title="Cadastrar anime" />
-      </DialogTrigger>
-      <DialogContent className="md:max-w-7xl w-[calc(100%-2rem)] p-0 overflow-hidden">
-        <DialogTitle className="sr-only">Cadastrar Anime</DialogTitle>
-        <DialogTitleStyled
-          title="Modulo de Anime"
-          description="Registrar"
-          descriptionReason="Anime"
-        >
-          <CollectionForm isLoading={isLoading} setIsLoading={setIsLoading} />
-        </DialogTitleStyled>
-      </DialogContent>
-    </Dialog>
+    <StyledDialog
+      title="Modulo de Anime"
+      description="Registrar"
+      descriptionReason="Anime"
+      childrenButton={<RegisterButton title="Cadastrar anime" />}
+    >
+      <CollectionForm isLoading={isLoading} setIsLoading={setIsLoading} />
+    </StyledDialog>
   );
 }
