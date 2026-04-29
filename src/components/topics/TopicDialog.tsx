65cf4de2e91db17aab7abfdf0b9809a45e582a4f
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-04-29 15:10:31 -0300

    fix: refactor topic dialog component

--- src/components/topics/TopicDialog.tsx
+++ src/components/topics/TopicDialog.tsx
@@ -1,41 +1,24 @@
 "use client";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
-import { DialogTitleStyled } from "../DialogTitleStyled";
 import { useState } from "react";
-import { RegisterButton } from "../RegisterButton";
 import { TopicForm } from "./TopicForm";
 import { IAnime } from "@/interfaces/IAnime";
+import { StyledDialog } from "../StyledDialog";
+import { RegisterButton } from "../RegisterButton";
 
 export function TopicDialog({ animes }: { animes: IAnime[] }) {
   const [isLoading, setIsLoading] = useState(false);
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <RegisterButton title="Cadastrar Tópico" />
-      </DialogTrigger>
-      <DialogContent className="md:max-w-7xl w-[calc(100%-2rem)] p-0 overflow-hidden">
-        <DialogTitle className="sr-only">Cadastrar Tópico</DialogTitle>
-        <DialogDescription className="sr-only">
-          Cadastrar Tópico
-        </DialogDescription>
-        <DialogTitleStyled
-          title="Modulo de Tópico"
-          description="Registrar"
-          descriptionReason="Tópico"
-        >
-          <TopicForm
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            animes={animes}
-          />
-        </DialogTitleStyled>
-      </DialogContent>
-    </Dialog>
+    <StyledDialog
+      title="Modulo de Tópico"
+      description="Registrar"
+      descriptionReason="Tópico"
+      childrenButton={<RegisterButton title="Cadastrar Tópico" />}
+    >
+      <TopicForm
+        isLoading={isLoading}
+        setIsLoading={setIsLoading}
+        animes={animes}
+      />
+    </StyledDialog>
   );
 }
