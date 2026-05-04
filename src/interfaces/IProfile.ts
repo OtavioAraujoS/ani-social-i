1a7afaa9ccde77bf2764999e875196f69dd3aa86
OtavioAraujoS <otavioaraujo490@gmail.com>
2026-05-04 13:07:45 -0300

    feat: create interface for profile topic section

---
+++ src/interfaces/IProfile.ts
@@ -0,0 +1,8 @@
+export interface IProfileTopics {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: Date | string | number;
+  updatedAt: Date | string | number;
+  comments: number;
+}
